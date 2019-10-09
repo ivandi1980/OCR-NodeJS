26e24172b5d11e57274535567a5b48fe1425e31a
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-09 17:57:07 +0800

    update style and fixed upload bugs

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Declare all Imports
 const express = require('express');
 const app = express();
 const fs = require('fs');
@@ -9,20 +10,32 @@ const worker = createWorker({
 });
 
 
+// Declare all Storage
 const storage = multer.diskStorage({
-    destination: (req, res, callback) => {
-        callback(null, './uploads');
+    destination: (req, file, cb) => {
+        cb(null, './uploads');
     },
-    filename: (req, res, callback) => {
-        callback(null, req.file);
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
     }
 });
 
 const upload = multer({ storage: storage }).single('Avatar');
 
+// Declare the View Engine
 app.set('view engine', 'ejs');
-// app.get('/upload', (req, res) => {
 
-// })
+// Declare the Routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        console.log(req.file);
+    });
+});
+
+
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on Port : ${PORT}`)); 
