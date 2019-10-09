4eb2fd7f5e31e8519d2fd19449c80264522ac973
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-09 17:07:42 +0800

    added views & update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('Server is now running...')
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const multer = require('multer');
+// const { tesseract }
+
+
+const storage = multer.diskStorage({
+    destination: (req, res, callback) => {
+        callback(null, './uploads');
+    },
+    filename: (req, res, callback) => {
+        callback(null, req.file);
+    }
+});
+
+const upload = multer({ storage: storage}).single('Avatar');
+
+app.set('view engine', 'ejs');
+// app.get('/upload', (req, res) => {
+
+// })
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on Port : ${PORT}`)); 
